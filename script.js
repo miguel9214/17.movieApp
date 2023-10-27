3b87d0b28e2385b472615775d09819ce42bfc55c
Miguel Ramos <miguel921433@gmail.com>
2023-10-27 14:02:41 -0500

    actualizado

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const API_URL =
-  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1";
-const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+  "https://api.mercadolibre.com/items/MCO926126955";
+const IMG_PATH = "https://http2.mlstatic.com/D_782361-MCO70444888881_072023-I.jpg";
 const SEARCH_API =
   'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="';
 
@@ -10,35 +10,47 @@ const main = document.getElementById("main");
 const search = document.getElementById("search");
 const form = document.getElementById("form");
 
+
+console.log('ESTA ES LA API',API_URL);
+
 getMovies(API_URL);
+
 
 async function getMovies(url) {
   const res = await fetch(url);
   const data = await res.json();
 
-  showMovies(data.results);
+  showMovies(data);
+
+  console.log(data)
 }
 
 async function showMovies(movies) {
   main.innerHTML = "";
 
-  for (const movie of movies) {
-    const { title, poster_path, vote_average, overview } = movie;
+  
+  for (const movie in movies) {
+    const { id, title, category_id, price,site_id } = movie;
+    movies.site_id;
+    console.log(movies.id);
+
+
+    console.log(movie);
 
     const movieEl = document.createElement("div");
     movieEl.classList.add("movie");
 
     // Traducir la descripción general
-    const translatedOverview = await translateText(overview, "en", "es");
+    // const translatedOverview = await translateText(title, "en", "es");
 
-    movieEl.innerHTML = `<img src="${IMG_PATH + poster_path}" alt="${title}">
+    movieEl.innerHTML = `<img src="${IMG_PATH + movies.site_id }" alt="${movies.id}">
       <div class="movie-info">
-        <h3>${title}</h3>
-        <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        <h3>${movies.seller_id}</h3>
+        <span class="${getClassByRate(title)}">${movies.title}</span>
       </div>
       <div class="overview">
         <h3>Descripción general</h3>
-        ${translatedOverview}
+        ${movies.price}
       </div>`;
     main.appendChild(movieEl);
   }
@@ -54,37 +66,37 @@ function getClassByRate(vote) {
   }
 }
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
 
-  const searchTerm = search.value;
+//   const searchTerm = search.value;
 
-  if (searchTerm && searchTerm !== "") {
-    getMovies(SEARCH_API + searchTerm);
+//   if (searchTerm && searchTerm !== "") {
+//     getMovies(SEARCH_API + searchTerm);
 
-    search.value = "";
-  } else {
-    window.location.reload();
-  }
-});
+//     search.value = "";
+//   } else {
+//     window.location.reload();
+//   }
+// });
 
-async function translateText(text, sourceLang, targetLang) {
-  const url = `https://translation.googleapis.com/language/translate/v2?key=${TRANSLATE_API_KEY}`;
+// async function translateText(text, sourceLang, targetLang) {
+//   const url = `https://translation.googleapis.com/language/translate/v2?key=${TRANSLATE_API_KEY}`;
 
-  const response = await fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      q: text,
-      source: sourceLang,
-      target: targetLang,
-      format: "text",
-    }),
-  });
+//   const response = await fetch(url, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({
+//       q: text,
+//       source: sourceLang,
+//       target: targetLang,
+//       format: "text",
+//     }),
+//   });
 
-  const data = await response.json();
+//   const data = await response.json();
 
-  return data.data.translations[0].translatedText;
-}
+//   return data.data.translations[0].translatedText;
+// }
